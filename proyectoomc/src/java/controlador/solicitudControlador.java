73a9096ee00e89e915f1864d36f8af9a61164135
Aprendiz <christiam23_@hotmail.com>
2019-08-20 11:31:03 -0500

    Subida de archivos 20/08/2019 11:30

--- proyectoomc/src/java/controlador/solicitudControlador.java
+++ proyectoomc/src/java/controlador/solicitudControlador.java
@@ -11,11 +11,16 @@ import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
 import facade.*;
 import entidades.*;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.sql.DriverManager;
 import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.Part;
 
 /**
  *
@@ -25,19 +30,46 @@ import javax.faces.application.FacesMessage;
 @SessionScoped
 public class solicitudControlador implements Serializable {
 
+    private Part file;
+    private String nombre;
+    private String pathReal;
+
+    public Part getFile() {
+        return file;
+    }
+
+    public void setFile(Part file) {
+        this.file = file;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPathReal() {
+        return pathReal;
+    }
+
+    public void setPathReal(String pathReal) {
+        this.pathReal = pathReal;
+    }
+
     /**
      * Creates a new instance of solicitud
      */
     public solicitudControlador() {
     }
-     @EJB
+    @EJB
     private ClienteFacade clienteFacade;
     Cliente cliente;
 
     @EJB
     private SolicitudFacade solicitudFacade;
-    Solicitud solicitud ;
-
+    Solicitud solicitud;
 
     @PostConstruct
     public void init() {
@@ -61,8 +93,6 @@ public class solicitudControlador implements Serializable {
         this.solicitud = solicitud;
     }
 
-    
-
     //Metodos
     public List<Solicitud> consultarTodos() {
         return solicitudFacade.findAll();
@@ -73,13 +103,13 @@ public class solicitudControlador implements Serializable {
     }
 
     public String registrarSolicitud() {
-        
+
         solicitud.setClienteIdCliente(clienteFacade.find(cliente.getIdCliente()));
         solicitudFacade.create(solicitud);
         solicitud = new Solicitud();
         return "listaSolicitud";
 
-  }
+    }
 
     public void eliminarSolicitud(Solicitud s) {
         this.solicitudFacade.remove(s);
@@ -96,5 +126,33 @@ public class solicitudControlador implements Serializable {
         solicitud = new Solicitud();
         return "listaSolicitud";
     }
-   
+
+    public String upload() {
+
+        String path = FacesContext.getCurrentInstance().getExternalContext().getRealPath("Archivos");
+        path = path.substring(0, path.indexOf("\\build"));
+        path = path + "\\web\\Archivos\\";
+        try {
+            this.nombre = file.getSubmittedFileName();
+            pathReal = "/proyectoomc/archivos/" + nombre;
+            path = path + this.nombre;
+            InputStream in = file.getInputStream();
+
+            byte[] data = new byte[in.available()];
+            in.read(data);
+            FileOutputStream out = new FileOutputStream(new File(path));
+            out.write(data);
+            in.close();
+            out.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        solicitud.setClienteIdCliente(getCliente());
+        this.solicitud.setFormula(pathReal);
+        solicitudFacade.create(solicitud);
+        solicitud = new Solicitud();
+
+        return "listaSolicitud";
+
+    }
 }
