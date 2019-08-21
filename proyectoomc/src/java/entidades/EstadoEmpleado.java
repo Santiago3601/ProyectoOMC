88a7e483d0daca6b1e2a83853ba2e56da49189fc
Aprendiz <christiam23_@hotmail.com>
2019-08-21 10:39:50 -0500

    Menu dinamico completo y base de datos

---
+++ proyectoomc/src/java/entidades/EstadoEmpleado.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Aprendiz
+ */
+@Entity
+@Table(name = "estado_empleado")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "EstadoEmpleado.findAll", query = "SELECT e FROM EstadoEmpleado e")
+    , @NamedQuery(name = "EstadoEmpleado.findByIdEstadoEmpleado", query = "SELECT e FROM EstadoEmpleado e WHERE e.idEstadoEmpleado = :idEstadoEmpleado")
+    , @NamedQuery(name = "EstadoEmpleado.findByEstado", query = "SELECT e FROM EstadoEmpleado e WHERE e.estado = :estado")})
+public class EstadoEmpleado implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "id_estado_empleado")
+    private Integer idEstadoEmpleado;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 50)
+    @Column(name = "estado")
+    private String estado;
+
+    public EstadoEmpleado() {
+    }
+
+    public EstadoEmpleado(Integer idEstadoEmpleado) {
+        this.idEstadoEmpleado = idEstadoEmpleado;
+    }
+
+    public EstadoEmpleado(Integer idEstadoEmpleado, String estado) {
+        this.idEstadoEmpleado = idEstadoEmpleado;
+        this.estado = estado;
+    }
+
+    public Integer getIdEstadoEmpleado() {
+        return idEstadoEmpleado;
+    }
+
+    public void setIdEstadoEmpleado(Integer idEstadoEmpleado) {
+        this.idEstadoEmpleado = idEstadoEmpleado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idEstadoEmpleado != null ? idEstadoEmpleado.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof EstadoEmpleado)) {
+            return false;
+        }
+        EstadoEmpleado other = (EstadoEmpleado) object;
+        if ((this.idEstadoEmpleado == null && other.idEstadoEmpleado != null) || (this.idEstadoEmpleado != null && !this.idEstadoEmpleado.equals(other.idEstadoEmpleado))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entidades.EstadoEmpleado[ idEstadoEmpleado=" + idEstadoEmpleado + " ]";
+    }
+    
+}
---
+++ proyectoomc/src/java/facade/EstadoEmpleadoFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facade;
+
+import entidades.EstadoEmpleado;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Aprendiz
+ */
+@Stateless
+public class EstadoEmpleadoFacade extends AbstractFacade<EstadoEmpleado> {
+
+    @PersistenceContext(unitName = "proyectoOMCPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public EstadoEmpleadoFacade() {
+        super(EstadoEmpleado.class);
+    }
+    
+}
